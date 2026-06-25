28d46775b51700d5fe320343fed04f8d92dec3fc
Dirige <1752256014@qq.com>
2026-06-25 18:45:59 +0800

    fix: replace deprecated android.net.NetworkInterface with java.net.NetworkInterface

--- server/src/main/java/com/phonemirror/server/MainActivity.kt
+++ server/src/main/java/com/phonemirror/server/MainActivity.kt
@@ -7,7 +7,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.media.projection.MediaProjectionManager
-import android.net.NetworkInterface
+import java.net.NetworkInterface
 import android.os.Build
 import android.os.Bundle
 import android.view.View
